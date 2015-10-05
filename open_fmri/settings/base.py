b0c80dfd5350b0045d209f006b26881f5b06e8b4
Ross Blair <rblair@Rosss-iMac.local>
2015-10-05 14:36:20 -0700

    initial commit with project setup using https://github.com/marcofucci/cookiecutter-simple-django. jenkins requirements file removed.

---
+++ open_fmri/settings/base.py
@@ -0,0 +1,108 @@
+import sys
+from os.path import join, abspath, dirname
+
+# PATH vars
+
+here = lambda *x: join(abspath(dirname(__file__)), *x)
+PROJECT_ROOT = here("..")
+root = lambda *x: join(abspath(PROJECT_ROOT), *x)
+
+sys.path.insert(0, root('apps'))
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'CHANGE THIS!!!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = []
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
+)
+
+PROJECT_APPS = ()
+
+INSTALLED_APPS += PROJECT_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'open_fmri.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'open_fmri.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'open_fmri',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-gb'
+
+TIME_ZONE = 'UTC'  # 'Europe/London'
+
+USE_I18N = False
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = root('assets', 'uploads')
+MEDIA_URL = '/media/'
+
+# Additional locations of static files
+
+STATICFILES_DIRS = (
+    root('assets'),
+)
+
+TEMPLATE_DIRS = (
+    root('templates'),
+)
+
+
+# .local.py overrides all the common settings.
+try:
+    from .local import *
+except ImportError:
+    pass
+
+
+# importing test settings file if necessary
+if len(sys.argv) > 1 and 'test' in sys.argv[1]:
+    from .testing import *
