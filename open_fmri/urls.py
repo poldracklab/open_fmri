0bbf3bd1b8bc52e84fff40f0afc351fbb9e8c122
Ross Blair <rblair@Rosss-iMac.local>
2015-10-05 16:18:56 -0700

    fixed broken nginx configuration to propelry point at uwsgi server

--- open_fmri/urls.py
+++ open_fmri/urls.py
@@ -4,9 +4,5 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tt.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
