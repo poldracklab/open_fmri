4a5b99a96fdfad1842694d211dcd18c4c5578cd7
Ross Blair <rblair2@stanford.edu>
2015-10-19 12:46:33 -0700

    added try except to featured article context processor to catch when there are no featured articles. Base template now checks to see if a featured article was returned from this context processor before displaying the featured article div

--- open_fmri/apps/dataset/context_processors.py
+++ open_fmri/apps/dataset/context_processors.py
@@ -1,5 +1,8 @@
 from dataset.models import FeaturedDataset
 
 def featured_dataset_processor(request):
-    featured_dataset = FeaturedDataset.objects.latest('date_featured')
-    return {'featured_dataset': featured_dataset}
+    try:
+        featured_dataset = FeaturedDataset.objects.latest('date_featured')
+        return {'featured_dataset': featured_dataset}
+    except FeaturedDataset.DoesNotExist:
+        return {}
