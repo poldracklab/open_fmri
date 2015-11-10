f07f829c0ea59515188e7d01dd54e9a360edc28d
Ross Blair <rblair2@stanford.edu>
2015-11-10 12:31:01 -0800

    added full pathing to all files in after_install. cd does not appear to be working as expected any more

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -18,19 +18,18 @@ cd /home/ec2-user/open_fmri
 sudo /etc/init.d/docker start
 
 # generate self signed certs used by nginx
-sudo openssl req -x509 -nodes -newkey rsa:2048 -subj "/C=US/ST=/L=/O=/CN=" -keyout ./nginx.key -out ./nginx.crt
-chown ec2-user. nginx.key nginx.crt
+sudo openssl req -x509 -nodes -newkey rsa:2048 -subj "/C=US/ST=/L=/O=/CN=" -keyout /home/ec2-user/open_fmri/nginx.key -out /home/ec2-user/nginx.crt
+chown ec2-user. /home/ec2-user/open_fmri/nginx.key /home/ec2-user/open_fmri/nginx.crt
 
 # use apg to generate a password and write it plain text to a file in our home 
 # directory and then use it to generate a htpasswd file. Password protection is
 # just a deterent from the site being wandered upon right now. For actual 
 # security with have proper logins setup in Django
 PASS=$(apg -n 1)
-sudo echo $PASS > ../.user_pass
-sudo htpasswd -nb admin $PASS > ./.htpasswd
+sudo echo $PASS > /home/ec2-user/.user_pass
+sudo htpasswd -nb admin $PASS > /home/ec2-user/open_fmri/.htpasswd
 
 # set up environment variables used by docker
-sudo mv env_example .env
-sudo sed -i 's/postgres_pass/$(apg -n 1 -m 100)/g' .env
-sudo sed -i 's/secret_key/$(apg -n 1 -m 100)/g' .env
-
+sudo mv /home/ec2-user/open_fmri/env_example /home/ec2-user/open_fmri/.env
+sudo sed -i 's/postgres_pass/$(apg -n 1 -m 100)/g' /home/ec2-user/open_fmri/.env
+sudo sed -i 's/secret_key/$(apg -n 1 -m 100)/g' /home/ec2-user/open_fmri/.env
