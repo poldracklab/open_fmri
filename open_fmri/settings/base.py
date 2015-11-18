a8833f8933bc8385c6b56278803e55ea4704f8eb
Ross Blair <rblair2@stanford.edu>
2015-11-18 11:16:18 -0800

    changed base settings file to pull secret key from environment variable by default

--- open_fmri/settings/base.py
+++ open_fmri/settings/base.py
@@ -12,7 +12,7 @@ sys.path.insert(0, root('apps'))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'CHANGE THIS!!!'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
