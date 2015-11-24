6d03b0c852c945fa7e27489863ccc98fe73a8530
Ross Blair <rblair2@stanford.edu>
2015-11-24 15:29:23 -0800

    updated task serializer to provide url and task name

--- open_fmri/apps/dataset/serializers.py
+++ open_fmri/apps/dataset/serializers.py
@@ -32,7 +32,7 @@ class RevisionSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['cogat_id', 'number']
+        fields = ['cogat_id', 'number', 'name', 'url']
 
 class DatasetSerializer(serializers.ModelSerializer):
     
