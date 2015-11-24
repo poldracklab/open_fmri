141427042ead9abecc473412a4f35b0870cd72fb
Ross Blair <rblair2@stanford.edu>
2015-11-24 15:08:42 -0800

    moved over some rest framework settings I had in local.py to base.py

--- open_fmri/settings/base.py
+++ open_fmri/settings/base.py
@@ -138,6 +138,11 @@ EMAIL_PORT = os.environ["EMAIL_PORT"]
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.SessionAuthentication',
+),}
+
 CKEDITOR_CONFIGS = {
     'default': {
         'width': '100%',
