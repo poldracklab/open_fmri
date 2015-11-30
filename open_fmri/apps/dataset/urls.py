476c82d6dcc1184816ce5c711a5bd4c875a8fba4
Ross Blair <rblair2@stanford.edu>
2015-11-30 13:07:42 -0800

    dataset references all switched over to accession number. old primary key patterns left in place for the time being

--- open_fmri/apps/dataset/urls.py
+++ open_fmri/apps/dataset/urls.py
@@ -7,26 +7,82 @@ from dataset.views import DatasetCreate, DatasetDelete, DatasetDetail, \
 from dataset.api_views import DatasetAPIList, DatasetAPIDetail
 
 urlpatterns = patterns('',
-    url(r'^new/$', DatasetCreate.as_view(), name='dataset_create'),
-    url(r'^edit/(?P<pk>\d+)$', DatasetUpdate.as_view(), name='dataset_update'),
-    url(r'^view/(?P<pk>\d+)$', DatasetDetail.as_view(), name='dataset_detail'),
+    url(
+        r'^new/$',
+        DatasetCreate.as_view(),
+        name='dataset_create'
+    ),
+    url(
+        r'^edit/(?P<pk>\d+)$',
+        DatasetUpdate.as_view(),
+        name='dataset_update'
+    ),
+    url(
+        r'^edit/(?P<acc_num>ds\d+[a-zA-Z]?)$',
+        DatasetUpdate.as_view(),
+        name='dataset_update'
+    ),
+    url(
+        r'^view/(?P<pk>\d+)$',
+        DatasetDetail.as_view(),
+        name='dataset_detail'
+    ),
     # this url maintains backwards compatability with old site detail view
-    url(r'^(?P<acc_num>ds\d+[a-zA-Z]?)/$', DatasetDetail.as_view(), name='dataset_detail'),
-    url(r'^delete/(?P<pk>\d+)$', DatasetDelete.as_view(), 
-        name='dataset_delete'),
-    url(r'^featured/delete/(?P<pk>\d+)$', 
-        FeaturedDatasetDelete.as_view(), name='featureddataset_delete'),
-    url(r'^featured/$', FeaturedDatasetEdit.as_view(), 
-        name='featureddataset_edit'),
-    url(r'^user_data_request/new/$', UserDataRequestCreate.as_view(),
-        name='user_data_request'),
-    url(r'^new/(?P<token>.*)$', UserCreateDataset.as_view(),
-        name='user_create_dataset'),
-    url(r'^$', DatasetList.as_view(), name='dataset_list'),
+    url(
+        r'^(?P<acc_num>ds\d+[a-zA-Z]?)/$',
+        DatasetDetail.as_view(),
+        name='dataset_detail'
+    ),
+    url(
+        r'^delete/(?P<pk>\d+)$',
+        DatasetDelete.as_view(), 
+        name='dataset_delete'
+    ),
+    url(
+        r'^delete/(?P<acc_num>ds\d+[a-zA-Z]?)/$', 
+        DatasetDelete.as_view(), 
+        name='dataset_delete'
+    ),
+    url(
+        r'^featured/delete/(?P<pk>\d+)$', 
+        FeaturedDatasetDelete.as_view(),
+        name='featureddataset_delete'
+    ),
+    url(
+        r'^featured/$',
+        FeaturedDatasetEdit.as_view(), 
+        name='featureddataset_edit'
+    ),
+    url(
+        r'^user_data_request/new/$',
+        UserDataRequestCreate.as_view(),
+        name='user_data_request'
+    ),
+    url(
+        r'^new/(?P<token>.*)$',
+        UserCreateDataset.as_view(),
+        name='user_create_dataset'
+    ),
+    url(
+        r'^$',
+        DatasetList.as_view(),
+        name='dataset_list'
+    ),
 )
 
 urlpatterns += patterns('',
-    url(r'^api/$', DatasetAPIList.as_view(), name='dataset_api_list'),
-    url(r'^api/(?P<acc_num>ds\d+[a-zA-Z]?)/$', DatasetAPIDetail.as_view(), name='dataset_api_detail'),
-    url(r'^api/(?P<pk>\d+)$', DatasetAPIDetail.as_view(), name='dataset_api_detail'),
+    url(
+        r'^api/$',
+        DatasetAPIList.as_view(),
+        name='dataset_api_list'
+    ),
+    url(
+        r'^api/(?P<acc_num>ds\d+[a-zA-Z]?)/$',
+        DatasetAPIDetail.as_view(),
+        name='dataset_api_detail'
+    ),
+    url(r'^api/(?P<pk>\d+)$',
+        DatasetAPIDetail.as_view(),
+        name='dataset_api_detail'
+    ),
 )
