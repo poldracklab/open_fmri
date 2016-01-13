a0eaa88aa2e4884c3e5d88a509f3f54850354459
Ross Blair <rblair2@stanford.edu>
2016-01-13 10:00:42 -0800

    added jquery to the mailling list subscrbe page to make the subscription request to the stanford server in the background and redirect client to a local success page

--- open_fmri/urls.py
+++ open_fmri/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.flatpages import views
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 
 import contact.urls
 import dataset.urls
@@ -20,5 +20,6 @@ urlpatterns = patterns('',
         name='data-organization'),
     url(r'^$', Index.as_view(), name='index'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^subscribed/', TemplateView.as_view(template_name='subscribe_success.html')),
     url(r'^(?P<url>.*/)$', views.flatpage),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
