24c8d55bd021a2dc296de3a305cd56449a15bdbe
Ross Blair <rblair2@stanford.edu>
2016-03-28 11:21:49 -0700

    added step missing from repo to concat letsencrypt intermediate cert to local signed cert

--- scripts/get_cert.sh
+++ scripts/get_cert.sh
@@ -10,13 +10,16 @@ cd /etc/letsencrypt/$DOMAIN
 openssl genrsa 4096 > account.key
 
 # Generate a domain private key (if you haven't already)
-openssl genrsa 4096 > domain.key
+openssl genrsa 4096 > nginx.key
 
 # Create a CSR for $DOMAIN
-openssl req -new -sha256 -key domain.key -subj "/CN=$DOMAIN" > domain.csr
+openssl req -new -sha256 -key nginx.key -subj "/CN=$DOMAIN" > domain.csr
 
 # Create the challenge folder in the webroot
 mkdir -p /var/www/.well-known/acme-challenge/
 
 # Get a signed certificate with acme-tiny
 python /opt/acme-tiny/acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /var/www/.well-known/acme-challenge/ > ./signed.crt
+
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem > intermediate.pem
+cat signed.crt intermediate.pem > nginx.crt
